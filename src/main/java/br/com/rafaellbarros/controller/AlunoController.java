a7c05472614551d0de59b944bfee2f1250dcba44
Rafael Barros <rafaelbarros.df@gmail.com>
2026-09-02 20:08:57 -0300

    ✨ feat: add openapi-generator, springdoc and mapstruct

---
+++ src/main/java/br/com/rafaellbarros/controller/AlunoController.java
@@ -0,0 +1,9 @@
+package br.com.rafaellbarros.controller;
+
+
+import br.com.rafaellbarros.api.AlunosApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlunoController implements AlunosApi {
+}
