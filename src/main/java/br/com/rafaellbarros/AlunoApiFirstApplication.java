067dd04a30edde4af79a54e84fd938b20fc9dcd1
Rafael Barros <rafaelbarros.df@gmail.com>
2026-09-02 17:51:00 -0300

    🎉 initial: initialize spring boot project with springdoc and lombok

---
+++ src/main/java/br/com/rafaellbarros/AlunoApiFirstApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaellbarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoApiFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoApiFirstApplication.class, args);
+	}
+
+}
